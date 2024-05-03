5539e8da1fd90f3d88cd437f24f79a522add8f25
itsmedona <donashaji254@gmail.com>
2024-05-03 14:58:59 +0530

    initial

---
+++ android/app/src/main/kotlin/com/example/payroll_ui_model_gtx/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.payroll_ui_model_gtx
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PayrollApp());
+}
+class PayrollApp extends StatelessWidget {
+  const PayrollApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
