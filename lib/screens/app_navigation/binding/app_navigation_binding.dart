d2e5ff03323ce5a6efb66dc2a88f5101d8af393b
itsmedona <donashaji254@gmail.com>
2024-05-25 10:53:06 +0530

    .

--- lib/screens/app_navigation/binding/app_navigation_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:payroll_ui_model_gtx/screens/app_navigation/controller/app_navigation_controller.dart';
-
-class AppNavigationBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => AppNavigationController());
-  }
-}
