21c5ac8ae94db292b4d353a60474d504292c68e4
itsmedona <donashaji254@gmail.com>
2024-05-25 11:01:04 +0530

    app routes set

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -6,18 +6,46 @@ import 'package:payroll_ui_model_gtx/screens/leave_request/binding/leave_request
 import 'package:payroll_ui_model_gtx/screens/leave_request/leave_request_screen.dart';
 import 'package:payroll_ui_model_gtx/screens/login/binding/login_binding.dart';
 import 'package:payroll_ui_model_gtx/screens/login/login_screen.dart';
+import 'package:payroll_ui_model_gtx/screens/login_with_otp_screen/bindings/login_with_otp_binding.dart';
+import 'package:payroll_ui_model_gtx/screens/login_with_otp_screen/login_with_otp.dart';
 import 'package:payroll_ui_model_gtx/screens/profile/bindings/profile_bindings.dart';
 import 'package:payroll_ui_model_gtx/screens/profile/profile_screen.dart';
+import 'package:payroll_ui_model_gtx/screens/signup/signup_details.dart';
+import 'package:payroll_ui_model_gtx/screens/signup_with_otp_screen/signup_with_otp.dart';
 
 import '../screens/attendence/bindings/attendence_screen_binding.dart';
 
 class AppRoutes {
+  static const String loginScreen = '/login_screen';
+  static const String loginWithOtpScreen = '/login_with_otp_screen';
+  static const String signupScreen = '/signup_details';
+  static const String signupWithOtpScreen = '/signup_with_otp_screen';
   static const String homeScreen = '/home_screen';
   static const String initialRoute = '/initialRoute';
   static const String attendenceScreen = '/attendence_screen';
   static const String profileScreen = '/profile_screen';
   static const String leaveRequestScreen = '/leave_request_screen';
   static List<GetPage> pages = [
+    GetPage(
+      name: loginScreen,
+      page: () => LoginScreen(),
+      bindings: [LoginBinding()],
+    ),
+    GetPage(
+      name: loginWithOtpScreen,
+      page: () => LoginWithOtpScreen(),
+      bindings: [LoginWithOtpBinding()],
+    ),
+    GetPage(
+      name: signupScreen,
+      page: () => SignupDetailsScreen(),
+      //bindings: [()],
+    ),
+    GetPage(
+      name: signupWithOtpScreen,
+      page: () => SignupWithOtpScreen(),
+      //bindings: [HomeScreenBinding()],
+    ),
     GetPage(
       name: homeScreen,
       page: () => HomeScreen(),
